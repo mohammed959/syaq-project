b9362eae936e40bdc013fdefea18f80a8f31b12e
mohammed nasser <44636863+mohammed959@users.noreply.github.com>
2023-11-19 11:49:15 +0300

    Update index.tsx
    change path

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/syaq-project">
     <App />
     </BrowserRouter>
   </React.StrictMode>
